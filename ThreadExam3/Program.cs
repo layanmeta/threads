ad2ec9584cfd985d6b977e7d50cfdec6fdd8234c
layan meta <layan.meta@gmail.com>
2021-03-07 17:29:40 +0200

    Add project files.

---
+++ ThreadExam3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadExam3
+{
+    class Program
+    {
+        public delegate int AddNumbers(int x, int y);
+
+        public Program()
+        {
+            AddNumbers sumDelegateOne = Add;
+            var result = sumDelegateOne.Invoke(1, 2);
+        }
+        int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static void Main(string[] args)
+        {
+            
+           
+        }
+    }
+}
